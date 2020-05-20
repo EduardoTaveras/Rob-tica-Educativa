4aa18c4584627f544460f341527b5f2e2769207f
EduardoTaveras <45051002+EduardoTaveras@users.noreply.github.com>
2020-05-19 22:16:09 -0400

    Add files via upload

---
+++ Robotica_resources/js/ardoraAlbumCFG.js
@@ -0,0 +1,31 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=20;
+var successes=0; successesMax=6; attempts=0; attemptsMax=2;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=1;
+var tiTime=true;
+var tiTimeType=2;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=false;
+var tiScore=true;
+var startTime;
+var colorBack="#D9FFE2"; colorButton="#0000FF"; colorText="#000000"; colorSele="#FFFFFF";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="'Arial Narrow', sans-serif";
+var fActi="'Arial Narrow', sans-serif";
+var fEnun="'Arial Narrow', sans-serif";
+var timeOnMessage=5; messageOk="Muy Bien"; messageTime=""; messageError="Error"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#00FF00"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var imaSel=0; texSel=0;
+var board=[["Robotica_resources/media/1.png","Robotica_resources/media/2.png","Robotica_resources/media/3.png","0"],["Robotica_resources/media/4.png","Robotica_resources/media/5.png","Robotica_resources/media/6.png","0"],["0","0","0","0"],["0","0","0","0"]]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=[]; answers=[["Microcontrolador","GiroSensor","Motor Pequeño","0"],["Táctil","Color","Motor Grande","0"],["0","0","0","0"],["0","0","0","0"]]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="Um9ib3RpY2E"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
